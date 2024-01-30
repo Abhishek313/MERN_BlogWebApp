0b921190c7faa2330d135bac21684f1599bd4784
Abhishek313 <vabhi5149@gmail.com>
2024-01-30 22:29:14 +0530

    created user model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
-import   express from "express";
+import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL)
+.then (() => console.log("connected"))
+.catch((err) => console.log(err));
 
 const app = express();
+
+app.get('/',(req,res) => {
+    res.send("hii");
+})
 
 
 app.listen(3000, () => {
